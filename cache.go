b478711a760659ef9184fd39da61ba25f105fb8b
Shimshir Emirdzan IU7-23B <shimshireao@student.bmstu.ru>
2022-07-27 14:18:22 +0300

    ttl test

--- cache.go
+++ cache.go
@@ -1,45 +1,63 @@
 package inMemoryCache
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type CacheMethods interface {
-	Set(key string, value interface{}) error
-	Get(key string) (interface{}, error)
+	Set(key string, value any, ttl time.Duration) error
+	Get(key string) (any, error)
 	Delete(key string) error
 }
 
+type data struct {
+	value      any
+	timeDelete time.Time
+}
+
 type Cache struct {
-	storage map[string]interface{}
+	storage map[string]data
 }
 
 func New() *Cache {
 	return &Cache{
-		storage: make(map[string]interface{}),
+		storage: make(map[string]data),
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}) error {
+func (c *Cache) Set(key string, value any, ttl time.Duration) error {
 	if key == "" {
 		return fmt.Errorf("empty key")
 	}
 
-	c.storage[key] = value
+	if ttl == 0 {
+		return fmt.Errorf("time-to-live equals zero")
+	}
+
+	c.storage[key] = data{value: value, timeDelete: time.Now().Add(ttl)}
 
 	return nil
 }
 
-func (c *Cache) Get(key string) (interface{}, error) {
+func (c *Cache) Get(key string) (any, error) {
 	if key == "" {
 		return nil, fmt.Errorf("empty key")
 	}
 
-	value, exists := c.storage[key]
+	data, exists := c.storage[key]
 
 	if !exists {
 		return nil, fmt.Errorf("no value for the key %s", key)
 	}
 
-	return value, nil
+	if time.Now().After(data.timeDelete) {
+		_ = c.Delete(key)
+
+		return nil, fmt.Errorf("no value for the key %s", key)
+	}
+
+	return data.value, nil
 }
 
 func (c *Cache) Delete(key string) error {
